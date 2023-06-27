42419623452ac2d92a1c2d7546664617ff1b2eba
Raja Soundaramourty <rajasoun@icloud.com>
2023-06-27 15:26:29 -0400

    brew file update

--- hello-service/.ci-cd/push.sh
+++ hello-service/.ci-cd/push.sh
@@ -2,4 +2,4 @@
 
 docker tag hello-service:latest registry.dev.local.gd:5001/hello-service:latest
 docker push registry.dev.local.gd:5001/hello-service:latest
-docker rmi registry.dev.local.gd:5001/hello-service:latest
+# docker rmi registry.dev.local.gd:5001/hello-service:latest
